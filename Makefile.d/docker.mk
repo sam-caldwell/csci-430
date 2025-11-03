98be4d715f17619a6f614021dcfef90b66bc418b
Sam Caldwell <github@samcaldwell.net>
2025-11-03 11:37:37 -0600

    fixes for the docker container to extend functionality

--- Makefile.d/docker.mk
+++ Makefile.d/docker.mk
@@ -7,7 +7,7 @@
 #  - docker/linux-image: Build the Docker image (ubuntu-based toolchain).
 #  - build/linux: Build and run the container, executing the full pipeline.
 
-.PHONY: docker/linux-image build/linux
+.PHONY: docker/linux-image build/linux lint/linux test/linux coverage/linux
 
 DOCKER_IMAGE ?= csci-430:linux
 
@@ -24,3 +24,33 @@ build/linux: docker/linux-image
 	  -w /work \
 	  $(DOCKER_IMAGE)
 
+# Run individual stages inside the container by overriding the entrypoint.
+lint/linux: docker/linux-image
+	@echo "[docker] Running lint in container..."
+	@docker run --rm -t \
+	  --entrypoint /bin/bash \
+	  -e GENERATOR=Ninja \
+	  -e TOOLCHAIN= \
+	  -v "$(PWD)":/work \
+	  -w /work \
+	  $(DOCKER_IMAGE) -lc "make clean configure lint"
+
+test/linux: docker/linux-image
+	@echo "[docker] Running tests in container..."
+	@docker run --rm -t \
+	  --entrypoint /bin/bash \
+	  -e GENERATOR=Ninja \
+	  -e TOOLCHAIN= \
+	  -v "$(PWD)":/work \
+	  -w /work \
+	  $(DOCKER_IMAGE) -lc "make clean configure test"
+
+coverage/linux: docker/linux-image
+	@echo "[docker] Running coverage in container..."
+	@docker run --rm -t \
+	  --entrypoint /bin/bash \
+	  -e GENERATOR=Ninja \
+	  -e TOOLCHAIN= \
+	  -v "$(PWD)":/work \
+	  -w /work \
+	  $(DOCKER_IMAGE) -lc "make clean configure coverage"
